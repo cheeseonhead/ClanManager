a856d30bae1782b309465f718c98a00d968204c0
Jeffrey Wu <jeffrey@tophat.com>
2017-01-14 06:30:21 -0500

    #3 | Pull request stuff

---
+++ pull-request.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Colors
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+JIRA_TICKET=`./scripts/ticket_extractor.rb`
+
+# select branch
+if [ $# -eq 0 ]
+    then # no branch provided
+        TARGET_BRANCH='develop'
+    else # use first argument as branch
+        TARGET_BRANCH="$1"
+fi
+
+# get current branch
+CUR_BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+TAR_BRANCH_SHORT=$(echo $TARGET_BRANCH | sed -e 's,.*/\(.*\),\1,')
+
+echo -e "Open PR to merge ${CYAN}$CUR_BRANCH${NC} into ${CYAN}$TAR_BRANCH_SHORT${NC}"
+
+echo -e "Merge $CUR_BRANCH into $TAR_BRANCH_SHORT" > testfile
+
+hub pull-request -F testFile -b tophatmonocle:$TARGET_BRANCH
+rm testFile
