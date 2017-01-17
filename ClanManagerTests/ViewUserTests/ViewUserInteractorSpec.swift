ae6acf8f8b8a69407c1619664cf8849df90c2461
Jeff Woo <cheeseonhead@gmail.com>
2017-01-17 00:05:42 -0500

    #18 | Added some tests

--- ClanManagerTests/ViewUserTests/ViewUserInteractorSpec.swift
+++ ClanManagerTests/ViewUserTests/ViewUserInteractorSpec.swift
@@ -15,9 +15,18 @@ class ViewUserInteractorSpec: QuickSpec
         describe("ViewUserInteractor")
         {
             var interactor: ViewUserInteractor!
+            var interactorSpy: ViewUserInteractorSpy!
+            var userWorkerSpy: UserWorkerSpy!
             beforeEach
             {
                 interactor = ViewUserInteractor()
+                interactorSpy = ViewUserInteractorSpy()
+                userWorkerSpy = UserWorkerSpy(userStore: UserMemStore())
+
+                userWorkerSpy.fakeResultUser = User(id: "thisIdExists", firstName: "Allen", lastName: "Chan", townHallLevel: 5)
+
+                interactor.output = interactorSpy
+                interactor.worker = userWorkerSpy
             }
 
             context("when asked to fetch user", {
@@ -25,7 +34,38 @@ class ViewUserInteractorSpec: QuickSpec
                 {
                     interactor.fetchUser(id: "thisIdNonExist", request: ViewUser.FetchUser.Request())
                 }
+
+                it("should trigger present user on output", closure: {
+                    expect(interactorSpy.presentUserCalled).toEventually(beTrue())
+                })
+
+                it("should request for the user with correct id from request", closure: {
+                })
             })
         }
+    }
+}
+
+fileprivate class ViewUserInteractorSpy: ViewUserInteractorOutput
+{
+    var presentUserCalled = false
+    var resultResponse: ViewUser.FetchUser.Response!
+
+    func presentUser(response: ViewUser.FetchUser.Response)
+    {
+        presentUserCalled = true
+        resultResponse = response
+    }
+}
+
+fileprivate class UserWorkerSpy: UserWorker
+{
+    var fetchUserCalled = false
+    var fakeResultUser: User!
+
+    override func fetchUser(id _: String, completionHandler: @escaping (_: User) -> Void)
+    {
+        fetchUserCalled = true
+        completionHandler(fakeResultUser)
     }
 }
