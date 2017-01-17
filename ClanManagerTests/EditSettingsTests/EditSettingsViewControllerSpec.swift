7bf194a597ef975941631bb40d08d00d933fb57c
Jeffrey Wu <jeffrey@tophat.com>
2017-01-17 17:28:12 -0500

    #26 | Added the spec file for ESVC

---
+++ ClanManagerTests/EditSettingsTests/EditSettingsViewControllerSpec.swift
@@ -0,0 +1,24 @@
+//
+//  EditSettingsViewControllerSpec.swift
+//  ClanManager
+//
+//  Created by Jeffrey Wu on 2017-01-17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class EditSettingsViewControllerSpec: QuickSpec {
+    
+    override func spec() {
+        describe("EditSettingsViewController") {
+            var viewController: EditSettingsViewController!
+            beforeEach {
+                viewController = EditSettingsViewController()
+            }
+        }
+    }
+    
+}
