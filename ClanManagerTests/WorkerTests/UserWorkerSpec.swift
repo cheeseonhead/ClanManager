af895a5ac313963ad020ef52f9acd912041fb309
Jeffrey Wu <jeffrey@tophat.com>
2017-01-17 00:49:13 -0500

    #22 | Finished the user worker spec

--- ClanManagerTests/WorkerTests/UserWorkerSpec.swift
+++ ClanManagerTests/WorkerTests/UserWorkerSpec.swift
@@ -22,17 +22,30 @@ class UserWorkerSpec: QuickSpec
             beforeEach
             {
                 userStoreSpy = UserMemStoreSpy()
+                userStoreSpy.fakeUser = User(id: "uniqueId", firstName: "Barry", lastName: "Allen", townHallLevel: 8)
+                
                 userWorker = UserWorker(userStore: userStoreSpy)
             }
 
             context("when asked to fetch a user", {
+                var resultUser: User!
                 beforeEach
                 {
-                    userWorker.fetchUser(id: "thisIdDoesntExist", completionHandler: { _ in })
+                    userWorker.fetchUser(id: "thisIdDoesntExist", completionHandler: { user in
+                        resultUser = user
+                    })
                 }
 
                 it("should have made a request to the store", closure: {
                     expect(userStoreSpy.fetchUserCalled).toEventually(beTrue(), timeout: Double(userStoreSpy.asyncDelaySeconds) + 0.1)
+                })
+                
+                it("should have made a request using the same id", closure: { 
+                    expect(userStoreSpy.requestWithId).toEventually(equal("thisIdDoesntExist"), timeout: Double(userStoreSpy.asyncDelaySeconds) + 0.1)
+                })
+                
+                it("should return with same user the store returned", closure: {
+                    expect(resultUser).toEventually(equal(userStoreSpy.fakeUser), timeout: Double(userStoreSpy.asyncDelaySeconds) + 0.1)
                 })
             })
         }
@@ -41,15 +54,22 @@ class UserWorkerSpec: QuickSpec
 
 fileprivate class UserMemStoreSpy: UserMemStore
 {
-    var fetchUserCalled = false
     var asyncDelaySeconds = 1
+    
+    // MARK: - Stub
+    var fakeUser: User!
+    
+    // MARK: - Spying
+    var fetchUserCalled = false
+    var requestWithId: String!
 
-    override func fetchUser(id _: String, completionHandler: @escaping (_: User) -> Void)
+    override func fetchUser(id: String, completionHandler: @escaping (_: User) -> Void)
     {
         fetchUserCalled = true
+        requestWithId = id
         let oneSecondAfter = DispatchTime.now() + DispatchTimeInterval.seconds(asyncDelaySeconds)
         DispatchQueue.main.asyncAfter(deadline: oneSecondAfter, execute: {
-            completionHandler(User())
+            completionHandler(self.fakeUser)
         })
     }
 }
