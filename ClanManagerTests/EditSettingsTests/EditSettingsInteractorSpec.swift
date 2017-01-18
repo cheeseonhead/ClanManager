8c61b352eb1b9de1f6263d876223b71034f971e5
Jeff Woo <cheeseonhead@gmail.com>
2017-01-18 01:55:13 -0500

    #27 | Wrote a test to check for request to worker

--- ClanManagerTests/EditSettingsTests/EditSettingsInteractorSpec.swift
+++ ClanManagerTests/EditSettingsTests/EditSettingsInteractorSpec.swift
@@ -17,10 +17,35 @@ class EditSettingsInteractorSpec: QuickSpec
         describe("EditSettingsInteractor")
         {
             var interactor: EditSettingsInteractor!
+            var workerSpy: SessionWorkerSpy!
             beforeEach
             {
+                workerSpy = SessionWorkerSpy()
                 interactor = EditSettingsInteractor()
+                interactor.sessionWorker = workerSpy
             }
+
+            context("when asked to fetch settings", {
+                beforeEach
+                {
+                    interactor.fetchSettings(request: EditSettings.FetchSettings.Request())
+                }
+
+                it("should send request to worker", closure: {
+                    expect(workerSpy.fetchSettingsCalled).toEventually(beTrue())
+                })
+            })
         }
+    }
+}
+
+fileprivate class SessionWorkerSpy: SessionWorker
+{
+    // Checks
+    var fetchSettingsCalled = false
+
+    override func fetchSettings(completionHandler _: @escaping (Settings?) -> Void)
+    {
+        fetchSettingsCalled = true
     }
 }
