502494f077688c7a44358262ff9409c098cd26e5
Jeff Woo <cheeseonhead@gmail.com>
2017-01-18 01:44:41 -0500

    #27 | Started a phantom sessionworker

---
+++ ClanManager/Workers/SessionWorker.swift
@@ -0,0 +1,17 @@
+//
+//  SessionWorker.swift
+//  ClanManager
+//
+//  Created by Cheese Onhead on 1/18/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import UIKit
+
+class SessionWorker
+{
+    func fetchSettings(completionHandler _: @escaping (_: User?) -> Void)
+    {
+
+    }
+}
