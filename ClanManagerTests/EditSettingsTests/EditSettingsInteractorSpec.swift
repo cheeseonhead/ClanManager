78e234335b97b48ffec7fba162135743e1733b27
Jeff Woo <cheeseonhead@gmail.com>
2017-01-18 01:38:21 -0500

    #27 | Added the spec file

---
+++ ClanManagerTests/EditSettingsTests/EditSettingsInteractorSpec.swift
@@ -0,0 +1,16 @@
+//
+//  EditSettingsInteractorSpec.swift
+//  ClanManager
+//
+//  Created by Cheese Onhead on 1/18/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class EditSettingsInteractorSpec: QuickSpec
+{
+
+}
