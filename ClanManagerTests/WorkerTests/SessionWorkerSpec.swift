7d4054a1d4c94cd14921ac1baf1809fd5f76975b
Jeffrey Wu <jeffrey@tophat.com>
2017-01-18 19:17:22 -0500

    #28 | Added the sessionWorkerSpec

---
+++ ClanManagerTests/WorkerTests/SessionWorkerSpec.swift
@@ -0,0 +1,18 @@
+//
+//  SessionWorkerSpec.swift
+//  ClanManager
+//
+//  Created by Jeffrey Wu on 2017-01-18.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class SessionWorkerSpec: QuickSpec
+{
+    override func spec()
+    {
+    }
+}
