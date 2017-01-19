1e0de5b9c0b8f3d9495b6d3d8340878760c046a7
Jeffrey Wu <jeffrey@tophat.com>
2017-01-19 16:13:11 -0500

    #30 | Setup the spec file for presenter

---
+++ ClanManagerTests/EditSettingsTests/EditSettingsPresenterSpec.swift
@@ -0,0 +1,24 @@
+//
+// Created by Jeffrey Wu on 2017-01-19.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class EditSettingsPresenterSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("EditSettingsPresenter")
+        {
+            var presenter: EditSettingsPresenter!
+
+            beforeEach
+            {
+                present = EditSettingsPresenter()
+            }
+        }
+    }
+}
