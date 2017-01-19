dbe566e40cb4dfdb32366a796b025c02e74d149a
Jeffrey Wu <jeffrey@tophat.com>
2017-01-19 13:46:27 -0500

    #28 | Setup the stubs and the second test for the fetchSettings

--- ClanManagerTests/WorkerTests/SessionWorkerSpec.swift
+++ ClanManagerTests/WorkerTests/SessionWorkerSpec.swift
@@ -18,23 +18,32 @@ class SessionWorkerSpec: QuickSpec
         {
             var worker: SessionWorker!
             var storeSpy: SessionStoreSpy!
+            var fakeSettings: Settings!
 
             beforeEach
             {
+                fakeSettings = Settings(currentPlayerTag: "fakePlayerTag")
                 worker = SessionWorker(store: SessionStoreSpy())
                 storeSpy = worker.store as! SessionStoreSpy
             }
 
             context("when asked to fetch settings")
             {
+                var resultSettings: Settings!
                 beforeEach
                 {
-                    worker.fetchSettings(completionHandler: { _ in })
+                    storeSpy.fakeSettings = fakeSettings
+                    worker.fetchSettings(completionHandler: { settings in resultSettings = settings })
                 }
 
                 it("should send a request to the store")
                 {
                     expect(storeSpy.fetchSettingsCalled).toEventually(beTrue())
+                }
+
+                it("should return same result from the store")
+                {
+                    expect(resultSettings).toEventually(equal(fakeSettings), timeout: Double(storeSpy.asyncDelaySeconds) + 0.1)
                 }
             }
         }
@@ -43,11 +52,20 @@ class SessionWorkerSpec: QuickSpec
 
 class SessionStoreSpy: SessionMemStore
 {
+    var asyncDelaySeconds = 1
+
     // Checks
     var fetchSettingsCalled = false
 
-    override func fetchSettings(completionHandler _: @escaping (Settings?) -> Void)
+    // Stubs
+    var fakeSettings: Settings!
+
+    override func fetchSettings(completionHandler: @escaping (Settings?) -> Void)
     {
         fetchSettingsCalled = true
+        let oneSecondAfter = DispatchTime.now() + DispatchTimeInterval.seconds(asyncDelaySeconds)
+        DispatchQueue.main.asyncAfter(deadline: oneSecondAfter, execute: {
+            completionHandler(self.fakeSettings)
+        })
     }
 }
