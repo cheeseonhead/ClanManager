49e830c0d35ecc88b617b1811448290be0820f5b
Jeffrey Wu <jeffrey@tophat.com>
2017-01-22 01:12:39 -0500

    #38 | Added the test for TCRouter

---
+++ ClanManagerTests/TabControlTests/TabControlRouterTests.swift
@@ -0,0 +1,19 @@
+//
+//  TabControlRouterTests.swift
+//  ClanManager
+//
+//  Created by Jeffrey Wu on 2017-01-22.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class TabControlRouterSpec: QuickSpec
+{
+    override func spec()
+    {
+
+    }
+}
