472a7eb293126f53fcdc13ae8cd1b41b8c861c2b
Jeff Woo <cheeseonhead@gmail.com>
2017-01-22 06:29:44 -0500

    #38 | Added controller spec

---
+++ TabControlViewControllerSpec.swift
@@ -0,0 +1,19 @@
+//
+//  TabControlViewControllerSpec.swift
+//  ClanManager
+//
+//  Created by Cheese Onhead on 1/22/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class TabControlViewControllerSpec: QuickSpec
+{
+    override func spec()
+    {
+
+    }
+}
