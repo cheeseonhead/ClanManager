dc0db9dba15fcbf81960bc8077ca9a63e1e059a6
Jeffrey Wu <jeffrey@tophat.com>
2017-01-24 03:30:55 -0500

    #38 | Added a testing check to the app delegate, to stop initializing the whole app on test startup

--- ClanManager/AppDelegate.swift
+++ ClanManager/AppDelegate.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
+func isRunningUnitTests() -> Bool
+{
+    return NSClassFromString("XCTestCase") != nil
+}
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
     var window: UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        if isRunningUnitTests()
+        {
+            print("TESTING")
+
+            // Create an empty window. Useful for unit tests that might need to alter the view heirarchy or
+            // add views during their tests.
+            let window = UIWindow(frame: UIScreen.main.bounds)
+            window.rootViewController = UIViewController()
+            window.makeKeyAndVisible()
+            self.window = window
+
+            return true
+        }
+
         BuddyBuildSDK.setup()
-        
-        // Override point for customization after application launch.
+
         return true
     }
 }
-
