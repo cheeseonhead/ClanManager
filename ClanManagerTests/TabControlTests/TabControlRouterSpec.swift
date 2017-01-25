7a9b5c0b6a511833da9d006e0a545d691a3ddf60
Jeffrey Wu <jeffrey@tophat.com>
2017-01-25 10:31:19 -0500

    #45 | Added the TabControlRouterSpec

---
+++ ClanManagerTests/TabControlTests/TabControlRouterSpec.swift
@@ -0,0 +1,22 @@
+//
+//  TabControlRouterSpec.swift
+//  ClanManager
+//
+//  Created by Jeffrey Wu on 2017-01-25.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+@testable import ClanManager
+import Quick
+import Nimble
+
+class TabControlRouterSpec: QuickSpec
+{
+    override func spec()
+    {
+        describe("TabControlRouter")
+        {
+
+        }
+    }
+}
