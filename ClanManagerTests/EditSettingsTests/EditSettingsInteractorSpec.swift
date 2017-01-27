fd50eaead475aaad111d45e333382ceef82bbe1e
Jeffrey Wu <jeffrey@tophat.com>
2017-01-26 19:06:22 -0500

    #52 | Added a whole bunch of tests to test the behaviour

--- ClanManagerTests/EditSettingsTests/EditSettingsInteractorSpec.swift
+++ ClanManagerTests/EditSettingsTests/EditSettingsInteractorSpec.swift
@@ -61,6 +61,83 @@ class EditSettingsInteractorSpec: QuickSpec
                     expect(outputSpy.gotResponse).toEventually(equal(EditSettings.FetchSettings.Response()))
                 }
             }
+
+            describe("when asked to store valid settings")
+            {
+                beforeEach
+                {
+                    let request = EditSettings.StoreSettings.Request(playerTag: "validPlayerTag")
+                    let fakeStoreSettingsResult = StoreSettingsResult()
+
+                    interactor.storeSettings(request: request)
+                    workerSpy.fakeStoreSettingsResult = fakeStoreSettingsResult
+                }
+
+                it("should call store settings on worker")
+                {
+                    expect(workerSpy.storeSettingsCalled).toEventually(beTrue())
+                }
+
+                it("should send correct version of settings to worker")
+                {
+                    let expected = Settings(currentPlayerTag: "validPlayerTag")
+                    expect(workerSpy.gotSettingsToStore).toEventually(equal(expected))
+                }
+
+                it("should trigger present on output")
+                {
+                    expect(outputSpy.presentStoreSettingsResultCalled).toEventually(beTrue())
+                }
+            }
+
+            describe("when asked to store invalid settings")
+            {
+                context("when player tag is empty")
+                {
+                    beforeEach
+                    {
+                        let request = EditSettings.StoreSettings.Request(playerTag: "")
+                        let fakeStoreSettingsResult = StoreSettingsResult(success: false, playerTagValidation: .errorEmpty)
+
+                        interactor.storeSettings(request: request)
+                        workerSpy.fakeStoreSettingsResult = fakeStoreSettingsResult
+                    }
+
+                    it("should not trigger method on worker")
+                    {
+                        expect(workerSpy.storeSettingsCalled).toNotEventually(beTrue())
+                    }
+
+                    it("should send appropriate response to output")
+                    {
+                        let expected = EditSettings.StoreSettings.Response(success: false, playerTagValidation: .empty)
+                        expect(outputSpy.gotStoreSettingsResponse).toEventually(equal(expected))
+                    }
+                }
+
+                context("when player tag contains spaces")
+                {
+                    beforeEach
+                    {
+                        let request = EditSettings.StoreSettings.Request(playerTag: "player tag")
+                        let fakeStoreSettingsResult = StoreSettingsResult(success: false, playerTagValidation: .errorContainsSpaces)
+
+                        interactor.storeSettings(request: request)
+                        workerSpy.fakeStoreSettingsResult = fakeStoreSettingsResult
+                    }
+
+                    it("should not trigger method on worker")
+                    {
+                        expect(workerSpy.storeSettingsCalled).toNotEventually(beTrue())
+                    }
+
+                    it("should send appropriate response to output")
+                    {
+                        let expected = EditSettings.StoreSettings.Response(success: false, playerTagValidation: .containsSpaces)
+                        expect(outputSpy.gotStoreSettingsResponse).toEventually(equal(expected))
+                    }
+                }
+            }
         }
     }
 }
@@ -87,7 +164,8 @@ fileprivate class SessionWorkerSpy: SessionWorker
         storeSettingsCalled = true
         gotSettingsToStore = settingsToStore
 
-        if fakeStoreSettingsResult != nil {
+        if fakeStoreSettingsResult != nil
+        {
             completionHandler(fakeStoreSettingsResult)
         }
     }
